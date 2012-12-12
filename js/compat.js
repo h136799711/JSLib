8afa36f8cc3639145722317cfd76465a4976db37
hbddingding <hebiduhebi@126.com>
2012-12-12 19:53:41 +0800

    rename to js/min/glMatrix.min.js

---
+++ js/compat.js
@@ -0,0 +1,8 @@
+window.requestAnimFrame = (function() {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+        function(callback, element) {
+            window.setTimeout(callback, 1000/60);
+        };
+})();
+
+

63cdda091e75d252fe318985abe5f2a9904d96e7
hbddingding <hebiduhebi@126.com>
2012-12-15 20:13:45 +0800

    remove compat.js webgl-utils.js input.js
    Signed-off-by: hbddingding <hebiduhebi@126.com>

--- js/compat.js
+++
@@ -1,8 +0,0 @@
-window.requestAnimFrame = (function() {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback, element) {
-            window.setTimeout(callback, 1000/60);
-        };
-})();
-
-
